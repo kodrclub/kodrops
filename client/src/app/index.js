00a11b8e44b9a8a4211fb49b08d49f7d6d6ab83f
Juan Manuel Medina <jmmedina@kodr.club>
2020-08-18 19:34:37 +0200

    The client's root route also displays the list of movies now

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,26 +1,23 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { NavBar } from '../components'
-import { MoviesList, MoviesInsert, MoviesUpdate } from '../pages'
+import { NavBar } from '../components';
+import { MoviesList, MoviesInsert, MoviesUpdate } from '../pages';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-    return (
-        <Router>
-            <NavBar />
-            <Switch>
-                <Route path="/movies/list" exact component={MoviesList} />
-                <Route path="/movies/create" exact component={MoviesInsert} />
-                <Route
-                    path="/movies/update/:id"
-                    exact
-                    component={MoviesUpdate}
-                />
-            </Switch>
-        </Router>
-    )
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact component={MoviesList} />
+        <Route path="/movies/list" exact component={MoviesList} />
+        <Route path="/movies/create" exact component={MoviesInsert} />
+        <Route path="/movies/update/:id" exact component={MoviesUpdate} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
