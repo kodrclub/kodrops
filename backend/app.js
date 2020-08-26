d1da5348ea39dd703bc65f8e3940d30df306681c
Juan Manuel Medina <jmmedina@kodr.club>
2020-08-26 15:44:19 +0200

    Made backend scrappable by Prometheus

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const prometheus = require('express-prometheus-middleware');
 
 const movieRouter = require('./routes/movie-router');
 
 const app = express();
 
+//
+// middlewares
+//
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
+app.use(
+  prometheus({
+    metricsPath: '/api/metrics',
+    // prefix: 'bcdop_backend_',
+    prefix: 'tlk_',
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+  })
+);
 
+//
+// routes
+//
 app.get('/api/version', (req, res) => {
   res.send(JSON.stringify({ version: app.version }));
 });
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,16 @@
 require('./db');
+// const promMid = require('express-prometheus-middleware');
+
 const app = require('./app').app;
+// app.use(
+//   promMid({
+//     metricsPath: '/api/metrics',
+//     // prefix: 'bcdop_backend_',
+//     prefix: 'tlkk_',
+//     collectDefaultMetrics: true,
+//     requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+//   })
+// );
 const apiPort = process.env.BACKEND_PORT || 3000;
 
 const version = process.env.npm_package_version;
