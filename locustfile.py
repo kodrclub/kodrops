634639838a7b1bdb433742c0f639941568d23729
Juan Manuel Medina <jmmedina@kodr.club>
2020-08-27 14:20:48 +0200

    Fine-tuning prometheus support

--- locustfile.py
+++ locustfile.py
@@ -6,12 +6,14 @@ class QuickstartUser(HttpUser):
 
     @task
     def index_page(self):
-        self.client.get("/")
+        for count in range(50):
+            self.client.get("/api/movies")
+            time.sleep(2)
 
     @task(3)
     def create_movie(self):
         ident = str(random.randint(1,1000))
-        for count in range(1000):
+        for count in range(100):
             self.client.post("/api/movie", {"name": "locust_%s_%s" % (ident, count),  "rating": random.randint(1,9)})
-            time.sleep(0.1)
+            time.sleep(1)
 
